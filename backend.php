cc15961fc97e5cc32559dc32d2d494618f721117
Terry Griffin <rugbyprof@users.noreply.github.com>
2016-04-04 16:00:59 -0700

    Create backend.php

---
+++ backend.php
@@ -0,0 +1,22 @@
+<?php
+
+$link = mysqli_connect("localhost", "msu2u", "msu2u2016!!!!!", "msu2u");
+
+$lat = $_POST['lat'];
+$lon = $_POST['lon'];
+$time = $_POST['time'];
+
+$command = $_POST['command'];
+
+switch($command){
+    case 'driver_location': 
+        $json = update_driver_location($lat,$lon,$time); 
+        break;
+    case default:
+        $json = array('error'=>'command not found');
+}
+
+
+function update_driver_location($link,$lat,$lon,$time){
+
+}
