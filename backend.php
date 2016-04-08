0d7ba33b0777ec79ae319e002d94027b0a318fe7
Johann <j_redhea1098@outlook.com>
2016-04-08 14:05:32 -0500

    adding my changes to workbranch

--- backend.php
+++ backend.php
@@ -1,13 +1,21 @@
 <?php
 error_reporting(0);
 
-$link = mysqli_connect("localhost", "msu2u", "msu2u2016!!!!!", "msu2u");
+$link = mysqli_connect('localhost', 'yam', 'yams', 'msu2u');
+if($link == false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
 
 $lat = $_POST['lat'];
 $lon = $_POST['lon'];
 $time = $_POST['time'];
 
 $command = $_POST['command'];
+$sql = "UPDATE users SET current_lat = $lat , current_lon = $lon , timestamp = '$time' WHERE id = 4";
+
+if($link->query($sql)=== TRUE) {echo "record updated";}
+else {echo "error updating" . $link->error;}
 
 switch($command){
     case 'driver_location': 
@@ -16,9 +24,13 @@ switch($command){
     default:
         $json = array('error'=>'command not found');
 }
-
 echo json_encode($json);
 
-function update_driver_location($link,$lat,$lon,$time){
-    return array('success'=>'you id it');
+function update_driver_location($lat,$lon,$time)
+{
+    return array('latitude,longitude'=>$lat,$lon,$time);
 }
+
+mysqli_close();
+?>
+
